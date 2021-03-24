5024ce49ff23d83d1d52edfd433df7c803d2d1d6
Jules <jfrancois968@gmail.com>
2021-03-24 14:51:16 +0100

    Q1 seance2

--- GamePedia/models/Character.php
+++ GamePedia/models/Character.php
@@ -12,9 +12,9 @@ class Character extends \Illuminate\Database\Eloquent\Model
 
     public function printCharacters($character_id) {
         foreach ($character_id as $char){
-            $res =  Character::query()->where('id', '=', $char)
-                ->select('id', 'deck')->get();
-            echo " - ".$res."</br>";
+            $res =  Character::query()->where('id', '=', $char["character_id"])
+                ->select('name', 'deck')->get();
+            echo " - ".$res[0]["name"]." : </br>".$res[0]["deck"]."</br>";
         }
 
 
